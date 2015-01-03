f164441983439c8060eded26cb504eb9ca7fdcba
Chris Rinaldi <cgrinaldi@gmail.com>
2015-01-03 16:00:23 -0500

    Add section on highlighting all matches

--- lesson.js
+++ lesson.js
@@ -42,6 +42,52 @@ aFunc("Mia");
 // it to something else. This is where multiple cursors can
 // come in handy.
 //
-// Exercise: Highlight `aFunc` everywhere using `Command + D`
-// and replace it with a better function name. Note how there
-// are multiple cursors!
+// Exercise: Highlight `aFunc` everywhere in the code
+// section using `Command + D` and replace it with a better
+// function name. Note how there are multiple cursors!
+
+// ##########################################
+// Highlighting All Matches -- Command + Ctrl + G
+// ##########################################
+
+// Highlighting the next match was fine if you just wanted
+// to replace a few names (while making sure you were truly
+// selecting the correct ones).
+//
+// However, you can imagine a time when you would want to
+// just do a global find all and replace. Of course you could
+// do this using Find and Replace (Shift + Command + F). So
+// let's give that a try:
+//
+// Exercise: Use Find and Replace to your change your function
+// name to something else.
+//
+// I don't know about you, but doing that was sort of a pain.
+// You probably had to click tab a couple of times, use the
+// mouse button, and even respond to menu prompts. We can do
+// much better!
+//
+// This time, let's try something similar to the previous,
+// with the key difference being that we will do the changes
+// all at once.
+
+aFunc("Chris");
+aFunc("Bob");
+aFunc("Sarah");
+aFunc("Mia");
+aFunc("Chris");
+aFunc("Bob");
+aFunc("Sarah");
+aFunc("Mia");
+aFunc("Chris");
+aFunc("Bob");
+aFunc("Sarah");
+aFunc("Mia");
+
+// Exercise: Place your cursor on your function name and press
+// Command + Ctrl + G. This will highlight every instance
+// of that word, and give you multiple cursors. Change the name
+// to something else.
+//
+// And that is the power of multiple cursors!
+
